9f62c26a9e035d86db5f32823b45b5f148d714af
Syohei YOSHIDA <syohex@gmail.com>
2014-02-22 17:50:23 +0900

    Bump up version and update Changes

--- helm-ack.el
+++ helm-ack.el
@@ -1,10 +1,10 @@
 ;;; helm-ack.el --- Ack command with helm interface
 
-;; Copyright (C) 2013 by Syohei YOSHIDA
+;; Copyright (C) 2014 by Syohei YOSHIDA
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-helm-ack
-;; Version: 0.06
+;; Version: 0.07
 ;; Package-Requires: ((helm "1.0") (cl-lib "0.4"))
 
 ;; This program is free software; you can redistribute it and/or modify
